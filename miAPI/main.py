a070fad15eb287beff14aae1f73ffab6814fa311
María Carmen Atilano García <123046171@upq.edu.mx>
2026-02-11 00:00:42 -0600

    Endpoint con Parámetros Obligatorios y Opcionales

--- miAPI/main.py
+++ miAPI/main.py
@@ -1,5 +1,5 @@
 #importaciones
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import asyncio
 
 #Instancia del servidor
@@ -16,3 +16,20 @@ async def bienvenido():
     return {"mensaje": "Bienvenido a FastAPI",
             "estatus" : "200",
             }
+
+#Endpoint con parametros obligatorios
+
+@app.get("/obligatorios")
+def obligatorios(nombre: str = Query(..., description="Nombre Obligatorio"), edad: int = 
+                 Query(..., description="Edad Obligatoria")):
+    return {"mensaje": f"Hola {nombre}, tu edad es {edad}"}
+
+#Endpoint con parametros opcionales
+
+@app.get("/opcionales")
+def opcionales(cidad: str = Query(None, description="Ciudad Opcional"), pais: str = Query(None, description="Pais Opcional")):
+    return {
+        "mensaje": "Datos recibidos",
+        "ciudad": cidad or "No especificado",
+        "pais": pais or "No especificado"
+    }
