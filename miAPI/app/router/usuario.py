6544c8fc06819f5a41bf9a3c130c713c39714960
María Carmen Atilano García <123046171@upq.edu.mx>
2026-03-24 15:16:58 -0600

    Practica no.9 - completa

--- miAPI/app/router/usuario.py
+++ miAPI/app/router/usuario.py
@@ -3,6 +3,11 @@ from app.models.usuario import crear_usuario
 from app.data.dtabase import usuarios
 from app.security.auth import verificar_peticion
 
+#Importaciones de BD
+from sqlalchemy.orm import Session
+from app.data.db import get_db 
+from app.data.usuarios import usuario as dbusuario
+
 router = APIRouter(
     prefix='/v1/usuarios',
     tags=['CRUD HTTP']
@@ -10,26 +15,27 @@ router = APIRouter(
 
 #GET
 @router.get("/")
-async def leer_usuarios():
+async def leer_usuarios(db: Session = Depends(get_db)):
+    queryUsuarios= db.query(dbusuario).all()
     return {
-        "total": len(usuarios),
-        "usuarios": usuarios,
+        "total": len(queryUsuarios),
+        "usuarios": queryUsuarios,
         "status": "200"
         }
 
 #POST
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def crear_usuario(usuario: crear_usuario):
-    for usr in usuarios:
-        if usr["id"] == usuario.id:
-            raise HTTPException(
-                status_code=400,
-                detail="El id ya existe"
-            )
-    usuarios.append(usuario)
+async def crear_usuario(usuarioP: crear_usuario, db: Session = Depends(get_db)):
+    nuevoU= dbusuario(
+        nombre=usuarioP.nombre,
+        edad=usuarioP.edad
+    )
+    db.add(nuevoU)
+    db.commit()
+    db.refresh(nuevoU)
     return {
         "mensaje": "Usuario Agregado",
-        "Usuario": usuario
+        "Usuario": usuarioP
     }
 
 #PUT
